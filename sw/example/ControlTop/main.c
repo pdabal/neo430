3821450e640c80d08fe184e2ef2ffc81e44c079b
Paweł Dąbal <pawel.dabal@wat.edu.pl>
2020-05-08 11:04:51 +0200

    Uruchomiony wariant oprogramowania dla CtrlWB v1.0

--- sw/example/ControlTop/main.c
+++ sw/example/ControlTop/main.c
@@ -1,30 +1,4 @@
-// #################################################################################################
-// #  < Blinking LED example program >                                                             #
-// # ********************************************************************************************* #
-// #  Displays an 8-bit counter on the high-active LEDs connected to the parallel output port.     #
-// # ********************************************************************************************* #
-// # This file is part of the NEO430 Processor project: https://github.com/stnolting/neo430        #
-// # Copyright by Stephan Nolting: stnolting@gmail.com                                             #
-// #                                                                                               #
-// # This source file may be used and distributed without restriction provided that this copyright #
-// # statement is not removed from the file and that any derivative work contains the original     #
-// # copyright notice and the associated disclaimer.                                               #
-// #                                                                                               #
-// # This source file is free software; you can redistribute it and/or modify it under the terms   #
-// # of the GNU Lesser General Public License as published by the Free Software Foundation,        #
-// # either version 3 of the License, or (at your option) any later version.                       #
-// #                                                                                               #
-// # This source is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;      #
-// # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     #
-// # See the GNU Lesser General Public License for more details.                                   #
-// #                                                                                               #
-// # You should have received a copy of the GNU Lesser General Public License along with this      #
-// # source; if not, download it from https://www.gnu.org/licenses/lgpl-3.0.en.html                #
-// # ********************************************************************************************* #
-// # Stephan Nolting, Hannover, Germany                                                 17.11.2018 #
-// #################################################################################################
-
-// Libraries
+// Biblioteki standardowe
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -33,17 +7,18 @@
 // Biblioteka modułów WB
 #include "wb_io.h"
 #include "wb_crypto.h"
-
+//
 #include "main.h"
 
-// Parametry interpretera wiersza poleceń
+// Stałe interpretera wiersza poleceń
 #define CMD_NUM 9
 #define CMD_LENGTH 5
-#define CMD_VALUE_LENGTH 1
+#define CMD_VALUE_LENGTH WB_CRYPTO_REG_VALUE_COUNT
+#define CMD_RESULT_LENGTH WB_CRYPTO_REG_RESULT_COUNT
 
-// Parametry interfejsu szeregowego
-#define BAUD_RATE 19200  // Prędkość transmisji USART
-#define BUFFER_LENGTH 70 // Długość bufora dla USART
+// Stałe interfejsu szeregowego
+#define BAUD_RATE 19200   // Prędkość transmisji USART
+#define BUFFER_LENGTH 128 // Długość bufora dla USART
 
 // Makra wiersza poleceń
 #define CMD buffer
@@ -51,11 +26,14 @@
 #define CMD_VALUE &buffer[CMD_LENGTH + 2]
 
 // Zmienne globalne
-char buffer[BUFFER_LENGTH]; // Bufor dla UART
-char *cmdList[CMD_NUM] = {  // Zbiór rozpoznawanych komend
+uint16_t value[CMD_VALUE_LENGTH]; // Bufor dla wartości VALUE/RESULT
+char buffer[BUFFER_LENGTH];       // Bufor dla UART
+char *cmdList[CMD_NUM] = {        // Zbiór rozpoznawanych komend
     ":STA", ":RST", ":CTR", ":VAL",
     ":RES", ":ACT", ":TRI",
     ":LED", ":DIP"};
+
+// #define USE_SPRINTF
 
 /* ------------------------------------------------------------
  * INFO Main function
@@ -64,151 +42,249 @@ int main(void)
 {
   uint16_t length;
 
-  memset(buffer, 0, BUFFER_LENGTH); // Zerowanie bufora odczytu
-
-  // Uruchomienie UART
-  neo430_uart_setup(BAUD_RATE);
-  neo430_uart_br_print("\nCtrlM - hello world\n");
-
-  // Inicjalizacja modułu CRYPTO
-  uint16_t crypto_init();
-
+  memset(buffer, 0, BUFFER_LENGTH);                // Zerowanie bufora odczytu UART
+  neo430_uart_setup(BAUD_RATE);                    // Uruchomienie UART
+  neo430_uart_br_print("\nCtrlM - hello world\n"); // komunikat powitalny
+  crypto_init();                                   // Inicjalizacja modułu CRYPTO
   while (1)
   {
     neo430_uart_br_print("\nCONTROL_DUT:> ");            // Znak zachęty dla komend - gotowość do pracy
     length = neo430_uart_scan(buffer, BUFFER_LENGTH, 0); // Wczytanie komendy
-    if (length < CMD_LENGTH)                             // Długość 0 oznacza ponowne wczytanie komendy po 10 ms
-    {
-      neo430_uart_br_print("\n\tCommand to short");
-      continue;
+    if (length < CMD_LENGTH)                             // Sprawdzenie minimalnej długości komendy
+    {                                                    // Za mała długość oznacza ponowne wczytanie komendy po 10 ms
+      neo430_uart_br_print("\n\tCommand to short");      // Informacja o błędzie
     }
     else // Próba wykonania komendy
     {
-      crypto_cmd(buffer);           // Wykonanie komendy
+      crypto_cmd(buffer);           // Wywołanie interpretera komend
       neo430_uart_br_print(buffer); // Wyświetlenie komunikatu zwrotnego z funkcji crypto_cmd
     }
   }
-  return 0;
 }
 
 //-----------------------------------------------------------------------------
-uint16_t crypto_init()
+// Inicjalizacja modułu CRYPTO
+void crypto_init()
 {
-  uint16_t result = 0, value = WB_CRYPTO_DEFAULT_INIT_VAL;
-  wb_crypto_reset();
-  wb_crypto_ctrl_set(0x0000);
-  wb_crypto_value_set(&value);
-  return result;
+  uint16_t value = WB_CRYPTO_DEFAULT_INIT_VAL;
+  wb_crypto_reset();              // Komenda RESET
+  wb_crypto_ctrl_set(0x0000);     // Wyzerowanie CTRL
+  wb_crypto_value_set(&value, 1); // Ustawienie startowe VALUE
 }
 
 // Przetwarzanie odebranych komend
-uint16_t crypto_cmd(char *buffer)
+void crypto_cmd(char *buffer)
 {
-  uint16_t result = 0;
-  uint16_t cmd = 0;
-  uint32_t value[CMD_VALUE_LENGTH];
+  uint16_t cmd = 0; // Numer komendy
 
-  // Wyczyszczenie pojemnika na wartości do wpisu odczytu
-  memset(value, 0, CMD_VALUE_LENGTH * sizeof(uint32_t));
-  // Zmiana wszystkich znaków na duże litery
-  CMD = strupr(buffer);
-  // Wykrycie przesłanej komendy
-  for (uint16_t i = 0; i < CMD_NUM; i++)
+  CMD = strupr(buffer);               // Zmiana wszystkich znaków na duże litery
+  for (cmd = 0; cmd < CMD_NUM; cmd++) // Przeszukiwanie listy komend
   {
-    if (!strncmp(CMD, cmdList[i], CMD_LENGTH - 1))
-    {
-      cmd = i + 1;
+    if (!strncmp(CMD, cmdList[cmd], CMD_LENGTH - 1)) // Pierwsze dopasowanie przerywa pętle
       break;
-    }
   }
-  // Sprawdzenie typu komendy - set/get
-  switch (CMD_TYPE)
+  switch (CMD_TYPE) // Sprawdzenie typu komendy - SET/GET
   {
-  case '=':                        // Ustawienie wartości
-    getCmdValue(CMD_VALUE, value); // Odczytanie przesłanej wartości z komendą =0x...
-    cmdSet(cmd, value);            // Wykonanie komendy
-    sprintf(buffer, "\nOK\n");
+  case '=':      // Komenda SET
+    cmdSet(cmd); // Wykonanie komendy
     break;
-  case '?': // Odczytanie wartości
-    memset(value, 0, CMD_VALUE_LENGTH * sizeof(uint32_t));
-    cmdGet(cmd, value); // Wykonanie komendy
-    setCmdValue(buffer, value, cmd);
+  case '?':      // Komenda GET
+    cmdGet(cmd); // Wykonanie komendy
     break;
-  default:
-    sprintf(buffer, "Nieprawidłowy parametr komendy");
-    result = 1;
-    break;
-  }
-  return result;
-}
-
-void cmdGet(uint16_t cmd, uint32_t *value)
-{
-  switch (cmd)
-  {
-  case 1:
-    *value = (uint32_t)wb_crypto_start_get();
-    break;
-  case 2:
-    *value = 0xCAFFE;
-    break;
-  case 3:
-    *value = (uint32_t)wb_crypto_ctrl_get();
-    break;
-  case 4:
-    wb_crypto_value_get((uint16_t *)value);
-    break;
-  case 5:
-    wb_crypto_result_get((uint16_t *)value);
-    break;
-  case 6:
-    *value = (uint32_t)wb_crypto_status_active_get();
-    break;
-  case 7:
-    *value = (uint32_t)wb_crypto_status_trig_get();
-    break;
-  case 8:
-    *value = (uint32_t)wb_io_led_get();
-    break;
-  case 9:
-    *value = (uint32_t)wb_io_dipsw_get();
-    break;
-  default:
+  default: // W każdym innym przypadku
+#ifndef USE_SPRINTF
+    sprintf(buffer, "Bad command parameter");
+#else
+    strcpy(buffer, "Bad command parameter");
+#endif
     break;
   }
 }
-
-void cmdSet(uint16_t cmd, uint32_t *value)
+// Interpretacja komend ustawiających
+void cmdSet(uint16_t cmd)
 {
+  uint16_t length = getCmdValue(CMD_VALUE, value); // Odczytanie przesłanej wartości z komendą =0x...
   switch (cmd)
   {
-  case 1:
-    wb_crypto_start_set((uint16_t)*value);
+  case 0:
+    wb_crypto_start_set(*value);
     break;
-  case 2:
+  case 1:
     wb_crypto_reset();
     break;
+  case 2:
+    wb_crypto_ctrl_set(*value);
+    break;
   case 3:
-    wb_crypto_ctrl_set((uint16_t)*value);
+    wb_crypto_value_set(value, length);
     break;
-  case 4:
-    wb_crypto_value_set((uint16_t *)value);
-    break;
-  case 8:
-    wb_io_led_set((uint16_t)*value);
+  case 7:
+    wb_io_led_set(*value);
     break;
   default:
+#ifndef USE_SPRINTF
+    sprintf(buffer, "\nBad command value\n");
+#else
+    strcpy(buffer, "\nBad command value\n");
+#endif
+#ifndef USE_SPRINTF
+    sprintf(buffer, "\nOK\n");
+#else
+    strcpy(buffer, "\nOK\n");
+#endif
     break;
   }
 }
 
 // Odczytanie wartości z bufora do tablicy value
-void getCmdValue(char *buffer, uint32_t *value)
+uint16_t getCmdValue(char *buffer, uint16_t *value)
 {
-  *value = neo430_hexstr_to_uint(buffer, strlen(buffer));
+  uint16_t length = strlen(buffer);     // Uzyskanie informacji o liczbie znaków w liczbie
+  length = (length > 64) ? 64 : length; // Sprawdzenie maksymalnej długości wartości do wczytania
+  uint16_t w = (length / 4);            // Liczba słów do wpisu
+  uint16_t wr = (length % 4);           // Sprawdzenie reszty z dzielenia
+  uint16_t ret = 0;
+  clear(value, CMD_VALUE_LENGTH); // Wyczyszczenie bufora do zapisu liczby
+  if (w == 0)                     // Jeżeli długość liczby mniejsza niż 4 znaki
+  {
+    *value = (uint16_t)neo430_hexstr_to_uint(buffer, wr); // Konwersja S2B
+    ret++;
+  }
+  else // Dla długości większej lub rówenej 4
+  {
+    uint16_t *tmp = value + w - 1;   // Określenie pozycji najbardziej znaczącego pełnego słowa
+    for (uint16_t i = 0; i < w; i++) // Odczytywanie kolejnych słów
+    {
+      if (wr != 0) // Jeżeli liczba znaków nie jest wielokrotnością 4 - pierwsza iteracja
+      {
+        tmp++;                                              // Wskazanie na kolejne słowo (niepełne)
+        *tmp = (uint16_t)neo430_hexstr_to_uint(buffer, wr); // Konwersja niepełnego słowa
+        buffer += wr;                                       // Przesunięcie wskaźnika o liczbę odczytanych znaków
+        tmp--;                                              // Wskazanie na młodsze słowo pełne
+        wr = 0;                                             // Wyzerowanie reszty
+        ret++;
+      }
+      // Wczytanie pełnego słowa
+      {
+        *tmp = (uint16_t)neo430_hexstr_to_uint(buffer, 4);
+        buffer += 4;
+        tmp--;
+      }
+    }
+  }
+  return (ret + w);
 }
 
-void setCmdValue(char *buffer, uint32_t *value, uint16_t cmd)
+void cmdGet(uint16_t cmd)
 {
-  sprintf(buffer, "\n%s = 0x%lx\n", cmdList[cmd - 1], *value);
+  switch (cmd)
+  {
+  case 0:
+    *value = (uint16_t)wb_crypto_start_get();
+    break;
+  case 1:
+    *value = (uint16_t)0xCAFFE;
+    break;
+  case 2:
+    *value = (uint16_t)wb_crypto_ctrl_get();
+    break;
+  case 3:
+    wb_crypto_value_get(value, CMD_VALUE_LENGTH);
+    break;
+  case 4:
+    wb_crypto_result_get(value);
+    break;
+  case 5:
+    *value = (uint16_t)wb_crypto_status_active_get();
+    break;
+  case 6:
+    *value = (uint16_t)wb_crypto_status_trig_get();
+    break;
+  case 7:
+    *value = (uint16_t)wb_io_led_get();
+    break;
+  case 8:
+    *value = (uint16_t)wb_io_dipsw_get();
+    break;
+  default:
+    break;
+  }
+  setCmdValue(buffer, value, cmd);
 }
+
+void setCmdValue(char *buffer, uint16_t *value, uint16_t cmd)
+{
+  uint32_t write = 0;
+#ifndef USE_SPRINTF
+  write = sprintf(buffer, "\n%s = 0x", cmdList[cmd]); // Nagłówek odpowiedzi
+#else
+  strcpy(buffer, "\n");
+  strcat(buffer, cmdList[cmd]);
+  strcat(buffer, " = 0x");
+#endif
+  if ((cmd != 3) && (cmd != 4)) // Dla komend innych niż :VAL? i :RES? zapis jednego słowa
+  {
+#ifndef USE_SPRINTF
+    sprintf(buffer + write, "%x", *value); // Zapisanie słowa do bufora
+#else
+    sprintf(buffer + write, "%x", *value); // Zapisanie słowa do bufora
+                                           // strcat(buffer, );
+#endif
+  }
+  else // Dla komend :VAL? i :RES? zapis określonej liczby słów
+  {
+    uint16_t n = (cmd == 3) ? CMD_VALUE_LENGTH : CMD_RESULT_LENGTH; // Określenie liczby słów do zapisania
+    uint16_t *tmp = value + n - 1;                                  // Określenie pozycji najbardziej znaczącego pełnego słowa
+    for (size_t i = 0; i < n; i++)
+    {
+#ifndef USE_SPRINTF
+      write += sprintf(buffer + write, "%.4x", *tmp);
+#else
+      write += sprintf(buffer + write, "%.4x", *tmp);
+      // strcat(buffer, );
+#endif
+      tmp--;
+    }
+  }
+  clear(value, CMD_VALUE_LENGTH); // Wyczyszczenie bufora liczby
+}
+
+void clear(uint16_t *value, uint16_t n)
+{
+  for (uint16_t i = 0; i < n; i++)
+  {
+    *value = 0;
+    value++;
+  }
+}
+#ifdef USE_SPRINTF
+static int num_hex_digits(unsigned n)
+{
+  if (!n)
+    return 1;
+
+  int ret = 0;
+  for (; n; n >>= 4)
+  {
+    ++ret;
+  }
+  return ret;
+}
+
+void make_hex_string_learning(unsigned n, char *s)
+{
+  const char hex_lookup[] = "0123456789abcdef";
+  int len = num_hex_digits(n);
+
+  if (len & 1)
+  {
+    *s++ = '0';
+  }
+  s[len] = '\0';
+
+  for (--len; len >= 0; n >>= 4, --len)
+  {
+    s[len] = hex_lookup[n & 0xf];
+  }
+}
+#endif
--- sw/example/ControlTop/main.h
+++ sw/example/ControlTop/main.h
@@ -3,15 +3,17 @@
 
 #include <neo430.h>
 
-void cmdSet(uint16_t cmd, uint32_t *value);
-void cmdGet(uint16_t cmd, uint32_t *value);
+void cmdSet(uint16_t cmd);
+void cmdGet(uint16_t cmd);
 
-void getCmdValue(char *buffer, uint32_t *value);
-void setCmdValue(char *buffer, uint32_t *value, uint16_t cmd);
+uint16_t getCmdValue(char *buffer, uint16_t *value);
+void setCmdValue(char *buffer, uint16_t *value, uint16_t cmd);
 
 uint16_t crypto_setValue(uint8_t *value, uint16_t n);
 
-uint16_t crypto_init();
-uint16_t crypto_cmd(char *buffer);
+void crypto_init();
+void crypto_cmd(char *buffer);
+
+void clear(uint16_t *value, uint16_t n);
 
 #endif
